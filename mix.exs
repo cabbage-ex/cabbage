be531a7eabb272a2cffab21efc9589c16746565b
Matt Widmann <mgwidmann@gmail.com>
2017-02-13 20:40:43 -0500

    Update to latest gherkin package.

--- mix.exs
+++ mix.exs
@@ -45,7 +45,7 @@ defmodule Cabbage.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:gherkin, "~> 1.0"},
+      {:gherkin, "~> 1.2"},
       {:ex_doc, "~> 0.10", only: :dev},
       {:earmark, "~> 0.1", only: :dev}
     ]
