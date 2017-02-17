0691951377a2eed1eaef7dcd46550c493c7115ef
Steven Blowers <steven@blowers.me>
2017-02-17 22:26:54 +0000

    missing step advisor converts numbers to regex capture groups

--- lib/cabbage/missing_step_advisor.ex
+++ lib/cabbage/missing_step_advisor.ex
@@ -1,14 +1,21 @@
 defmodule Cabbage.MissingStepAdvisor do
 
   def raise(step_text, step_type) do
+
+    converted_step_text = convert_numbers_to_regex_capture_group(step_text)
+
     raise """
     Please add a matching step for:
     "#{step_type} #{step_text}"
 
-      def#{step_type |> String.downcase} ~r/^#{step_text}$/, vars, state do
+      def#{step_type |> String.downcase} ~r/^#{converted_step_text}$/, vars, state do
         # Your implementation here
       end
     """
+  end
+
+  defp convert_numbers_to_regex_capture_group(step_text) do
+    Regex.replace(~r/(\s)\d+(\s|$)/, step_text, "\\1(?<number>\d+)\\2")
   end
 
 end
--- test/cabbage/missing_step_advisor_test.exs
+++ test/cabbage/missing_step_advisor_test.exs
@@ -3,7 +3,7 @@ defmodule Cabbage.MissingStepAdvisorTest do
 
   alias Cabbage.MissingStepAdvisor, as: Sut
 
-  test "raises RuntimeError is step is missing" do
+  test "raises RuntimeError if step is missing" do
     step_text = "I am Bob"
     step_type = "Given"
     expected_message = """
@@ -11,6 +11,40 @@ defmodule Cabbage.MissingStepAdvisorTest do
     "Given I am Bob"
 
       defgiven ~r/^I am Bob$/, vars, state do
+        # Your implementation here
+      end
+    """
+
+    assert_raise(RuntimeError,
+                 expected_message,
+                 fn -> Sut.raise(step_text, step_type) end)
+  end
+
+  test "convert numbers to regex capture group in message" do
+    step_text = "I am 49 years old"
+    step_type = "And"
+    expected_message = """
+    Please add a matching step for:
+    "And I am 49 years old"
+
+      defand ~r/^I am (?<number>\d+) years old$/, vars, state do
+        # Your implementation here
+      end
+    """
+
+    assert_raise(RuntimeError,
+                 expected_message,
+                 fn -> Sut.raise(step_text, step_type) end)
+  end
+
+  test "converting numbers to regex capture group ignores numbers with letters" do
+    step_text = "the 3rd number is 1101"
+    step_type = "When"
+    expected_message = """
+    Please add a matching step for:
+    "When the 3rd number is 1101"
+
+      defwhen ~r/^the 3rd number is (?<number>\d+)$/, vars, state do
         # Your implementation here
       end
     """
