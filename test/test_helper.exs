72033985cfaaa2b1bc812f41e9f584b218465f47
Roberts Guļāns <rgulans@gmail.com>
2018-11-19 05:52:03 +0100

    Testing: Last try to fixed 1.3 compatability

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -13,7 +13,7 @@ defmodule CabbageTestHelper do
   def run(filters \\ [], cases \\ [])
 
   def run(filters, module) do
-    {add_module, load_module} = versioned_callbacks()
+    {add_module, load_module, result_fix} = versioned_callbacks()
 
     Enum.each(module, add_module)
     load_module.()
@@ -24,15 +24,21 @@ defmodule CabbageTestHelper do
       |> Keyword.merge(colors: [enabled: false])
 
     output = capture_io(fn -> Process.put(:capture_result, ExUnit.Runner.run(opts, nil)) end)
-    {Map.merge(%{excluded: 0}, Process.get(:capture_result)), output}
+    {result_fix.(Process.get(:capture_result)), output}
   end
 
   defp versioned_callbacks() do
     System.version()
     |> Version.compare("1.6.0")
     |> case do
-      :lt -> {&ExUnit.Server.add_sync_case/1, &ExUnit.Server.cases_loaded/0}
-      _ -> {&ExUnit.Server.add_sync_module/1, &ExUnit.Server.modules_loaded/0}
+      :lt -> {&ExUnit.Server.add_sync_case/1, &ExUnit.Server.cases_loaded/0, &fix_13_elixir_test_result/1}
+      _ -> {&ExUnit.Server.add_sync_module/1, &ExUnit.Server.modules_loaded/0, &fix_17_elixir_test_result/1}
     end
+  end
+
+  defp fix_17_elixir_test_result(result), do: result
+
+  defp fix_13_elixir_test_result(result) do
+    Map.merge(result, %{excluded: Map.get(result, :skipped, 0), skipped: 0})
   end
 end
